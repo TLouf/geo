ecf40a688c0fa60607055231a42187a3b0bd24f4
TLouf <loufthomas@gmail.com>
2025-04-28 13:42:51 +0200

    fix: re

--- src/geo/h3.py
+++ src/geo/h3.py
@@ -7,7 +7,7 @@ from geo.dependencies import h3
 
 def to_polygon(h3_hex):
     """Convert an H3 hexagon index to a Shapely polygon."""
-    h3_shape = h3.cells_to_h3shape(list(h3_hex))
+    h3_shape = h3.cells_to_h3shape([h3_hex])
     return shapely.geometry.shape(h3_shape)
 
 
