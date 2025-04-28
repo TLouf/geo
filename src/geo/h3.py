73439b98625031f29b0b93bb97d4a90e41cdb2d7
TLouf <loufthomas@gmail.com>
2025-04-28 13:21:53 +0200

    fix for h3>4.x

--- src/geo/h3.py
+++ src/geo/h3.py
@@ -1,20 +1,20 @@
 import geopandas as gpd
 import pandas as pd
-from shapely.geometry import Polygon
+import shapely.geometry
 
 from geo.dependencies import h3
 
 
 def to_polygon(h3_hex):
     """Convert an H3 hexagon index to a Shapely polygon."""
-    boundary = h3.h3_to_geo_boundary(h3_hex, geo_json=True)
-    return Polygon(boundary)
+    h3_shape = h3.cells_to_h3shape(list(h3_hex))
+    return shapely.geometry.shape(h3_shape)
 
 
 def grid(area, level=9):
-    hexs = h3.polyfill(area.__geo_interface__, level, geo_json_conformant=True)
+    hexs = h3.geo_to_cells(area, level)
     grid = gpd.GeoDataFrame(
-        geometry=list(map(to_polygon, hexs)),
+        geometry=list(map(h3.cells_to_h3shape, hexs)),
         index=pd.Index(hexs, name="cell_id"),
         crs="EPSG:4326",
     )
